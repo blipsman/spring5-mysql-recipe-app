e7c01e3ea1874a57854f1c4b2459de2e52825424
blipsman <b_lipsman@yahoo.com>
2020-01-04 22:17:58 -0500

    created configure-mysql.sql file and added several commands

---
+++ src/main/scripts/configure-mysql.sql
@@ -0,0 +1,7 @@
+## Use to run mysql db docker image, optional if you're not using a local mysqldb
+# docker run --name mysqldb -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql
+
+# connect to mysql and run as root user
+#Create Databases
+CREATE DATABASE sfg_dev;
+CREATE DATABASE sfg_prod;
